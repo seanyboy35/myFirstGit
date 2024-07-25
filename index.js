4523184bf12e660a5def7a47c58b79ab909a508b
seanyboy35 <seandsokc35@gmail.com>
2024-07-25 12:04:39 +1000

    adding function

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("Hello World")
+function addingfunction() {
+    const num1 = 5
+    const num2 = 10
+    const sum = num1 + num2
+    return sum
+}
+const result = addingfunction();
+
+console.log("Result = $(result)")
+
+console.log(5+10)
